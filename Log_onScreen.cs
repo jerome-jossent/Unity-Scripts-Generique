3186a90db4cae24d1b0ffa660ab9593d7a6d9a96
jerome jossent <jerome.jossent@veolia.com>
2022-09-16 09:07:45 +0200

    Enum (next/previous), ViveTracker_NextDeviceIndex, Debug on GUI (Log_onScreen)

---
+++ Log_onScreen.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Log_onScreen : MonoBehaviour
+{
+    public uint max_messages = 15;  // number of messages to keep
+    Queue myLogQueue = new Queue();
+    public uint width = 400;
+    public int text_size = 20;
+    public KeyCode toucheSwitchShow;
+    public bool show;
+
+    void Start()
+    {
+        Debug.Log("Started up logging.");
+    }
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += HandleLog;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= HandleLog;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(toucheSwitchShow)) 
+            show = !show;
+    }
+    void HandleLog(string logString, string stackTrace, LogType type)
+    {
+        myLogQueue.Enqueue("[" + type + "] : " + logString);
+
+        if (type == LogType.Exception)
+            myLogQueue.Enqueue(stackTrace);
+
+        while (myLogQueue.Count > max_messages)
+            myLogQueue.Dequeue();
+    }
+
+    void OnGUI()
+    {
+        if (!show) return;
+
+        GUILayout.BeginArea(new Rect(Screen.width - width, 0, width, Screen.height));
+        //GUI.skin.label.fontSize = text_size;
+
+        GUIStyle myStyle = new GUIStyle();
+        myStyle.fontSize = text_size;
+        myStyle.wordWrap = true;
+        bool unsurdeux = true;
+        //GUILayout.Label("\n" + string.Join("\n", myLogQueue.ToArray()));
+
+        foreach (var item in myLogQueue)
+        {
+            unsurdeux = !unsurdeux;
+            myStyle.normal.textColor = (unsurdeux) ? Color.white : new Color(0.8f, 0.8f, 0.8f);
+            GUILayout.Label(item.ToString(), myStyle);
+        }
+
+
+        GUILayout.EndArea();
+    }
+}
---
+++ ViveTracker_NextDeviceIndex.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveTracker_NextDeviceIndex : MonoBehaviour
+{
+    public Valve.VR.SteamVR_TrackedObject tracker;
+    public KeyCode touchePrevious;
+    public KeyCode toucheNext;
+
+    private void OnEnable()
+    {
+        Debug.Log("Vive Tracker : find with " + toucheNext.ToString() + " & " + touchePrevious.ToString());
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(touchePrevious)) _Device_Previous();
+        if (Input.GetKeyDown(toucheNext)) _Device_Next();
+    }
+
+    public void _Device_Previous()
+    {
+        int newindex = (int)tracker.index.Previous();
+        tracker.SetDeviceIndex(newindex);
+    }
+    public void _Device_Next()
+    {
+        int newindex = (int)tracker.index.Next();
+        tracker.SetDeviceIndex(newindex);
+    }
+}
