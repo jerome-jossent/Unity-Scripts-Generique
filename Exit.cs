0a691f5fa782b6e137349083b0ea2c510649042d
jerome jossent <jerome.jossent@veolia.com>
2022-10-04 15:37:50 +0200

    exit pour WebGL (ouvre un autre onglet et sort du mode plein écran)

--- Exit.cs
+++ Exit.cs
@@ -20,10 +20,15 @@ public class Exit : MonoBehaviour {
 
     public void Quit()
     {
+#if (UNITY_EDITOR || DEVELOPMENT_BUILD)
+        Debug.Log(this.name + " : " + this.GetType() + " : " + System.Reflection.MethodBase.GetCurrentMethod().Name);
+#endif
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-#else
-		Application.Quit();
+#elif (UNITY_STANDALONE) 
+    Application.Quit();
+#elif (UNITY_WEBGL)
+    Application.OpenURL("about:blank");
 #endif
     }
 }
