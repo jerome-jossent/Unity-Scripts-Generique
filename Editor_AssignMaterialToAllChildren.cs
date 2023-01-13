7de40ac92cf77c57af85bc9fe41ef80323415770
jerome jossent <jerome.jossent@veolia.com>
2023-01-13 12:43:08 +0100

    Editor_AssignMaterialToAllChildren.cs

---
+++ Editor_AssignMaterialToAllChildren.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
+
+[ExecuteInEditMode]
+public class Editor_AssignMaterialToAllChildren : MonoBehaviour
+{
+    public bool GO;
+    public Material material_to_apply;
+
+    int count;
+    void OnValidate()
+    {
+        if (GO)
+        {
+            count = 0;
+            Debug.Log("Start");
+            APPLY(gameObject);
+            GO = false;
+            Debug.Log("End : " + count);
+        }
+    }
+
+    private void APPLY(GameObject go)
+    {
+        //lui-même
+        Renderer renderer = go.GetComponent<Renderer>();
+        if (renderer != null)
+        {
+            Material[] materials = go.GetComponent<Renderer>().sharedMaterials;
+            for (int i = 0; i < materials.Length; i++)
+                materials[i] = material_to_apply;
+            go.GetComponent<Renderer>().sharedMaterials = materials;
+            count++;
+        }
+
+        //ses enfants
+        for (int i = 0; i < go.transform.childCount; i++)
+            APPLY(go.transform.GetChild(i).gameObject);
+    }
+}
